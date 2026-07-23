6e04bfa84630c4f2f1c2597d4b89e92a9cda1717
guilherme-kunsch <guilherme.kunsch@outlook.com>
2026-07-23 08:30:31 -0300

    corrigindo a cor do qrcode

--- script.js
+++ script.js
@@ -287,8 +287,8 @@ function generateQRCode() {
             text: url,
             width: 300,
             height: 300,
-            colorDark: '#28a7468a',
-            colorLight: '#000000',
+            colorDark: '#000000',
+            colorLight: '#ffffff',
             correctLevel: QRCode.CorrectLevel.H
         });
         
